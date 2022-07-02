9494ef932fdc1c474bcb2ba97c79f22aadc49219
Carlos Rabelo <carlos.rabelo@ifmt.edu.br>
2022-07-02 14:14:20 -0400

    Ajustes nos scripts

--- lib/bin/profile.sh
+++ lib/bin/profile.sh
@@ -1,29 +1,35 @@
 #
 if [ -n "$BASH_VERSION" ]; then
+
     if [ -f "$HOME/.bashrc" ]; then
-	. "$HOME/.bashrc"
+
+	source "$HOME/.bashrc"
+
     fi
+
 fi
 
 #
-if [ -d "$HOME/bin" ] ; then
-    PATH="$HOME/bin:$PATH"
-fi
+if [ -d "$HOME/suap/.local/bin" ] ; then
 
-#
-if [ -d "$HOME/.local/bin" ] ; then
-    PATH="$HOME/.local/bin:$PATH"
-fi
+    PATH="$HOME/suap/.local/bin:$PATH"
 
-#
-if [ -f "$HOME/suap/.env/bin/activate" ] ; then
-    source $HOME/suap/.env/bin/activate
-    cd $HOME/suap
 fi
 
 #
 if [ -f "$HOME/suap/.vscode/password.sh" ] ; then
+
     source $HOME/suap/.vscode/password.sh
+
+fi
+
+#
+if [ -f "$HOME/suap/.env/bin/activate" ] ; then
+
+    source $HOME/suap/.env/bin/activate
+
+    cd $HOME/suap
+
 fi
 
 #
